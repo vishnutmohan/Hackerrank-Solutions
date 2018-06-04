e0556c9a462accd848408bda2d625782f1b724d8
Vishnu <vishnu@graspio.com>
2018-06-04 17:44:11 +0530

    adding anagram check code in python and node js

--- anagram.js
+++ anagram.js
@@ -1,12 +1,12 @@
 function findAnagramLength(string1, string2) {
     var difference = {};
     for (var i = 0; i < string1.length; i++) {
-        if (difference[string1.charAt(i)] == undefined)
+        if (difference[string1.charAt(i)] === undefined)
             difference[string1.charAt(i)] = 0
         difference[string1.charAt(i)] += 1
     }
-    for (var i = 0; i < string2.length; i++) {
-        if (difference[string2.charAt(i)] == undefined)
+    for (var i = 0; i < string2.length; i++) { 
+        if (difference[string2.charAt(i)] === undefined)
             difference[string2.charAt(i)] = 0
         difference[string2.charAt(i)] -= 1
     }
