c9064ea5e320eb3654e4eb03f1138281458c69df
Vishnu <vishnu@graspio.com>
2018-06-06 16:06:41 +0530

    linked list is done

---
+++ linked-list.py
@@ -0,0 +1,23 @@
+"""
+Detect a cycle in a linked list. Note that the head pointer may be 'None' if the list is empty.
+
+A Node is defined as: 
+ 
+    class Node(object):
+        def __init__(self, data = None, next_node = None):
+            self.data = data
+            self.next = next_node
+"""
+
+def has_cycle(head):
+    if head is None:
+        return False
+    slow = fast = head
+    while (slow or fast or fast.next):
+        if fast.next is None:
+            return False
+        if slow == fast.next or slow == fast.next.next:
+            return True
+        slow = slow.next
+        fast = fast.next.next
+    return False
